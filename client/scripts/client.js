d06fdbd9646b9121ed3bacc1662bd6ae9d2d7b5d
Bri Dickman <brianna.dickman@gmail.com>
2017-04-09 19:56:02 -0500

    Appending search movie to dom

--- client/scripts/client.js
+++ client/scripts/client.js
@@ -6,23 +6,24 @@ myApp.controller('FavoritesController', ['$scope', 'MovieService', function($sco
 }]);
 
 
+
 myApp.controller('SearchController', ['$scope', 'MovieService', function($scope, MovieService){
   console.log('SearchController');
 
   $scope.movieService = MovieService;
   $scope.movieSearch = '';
-  // $scope.movieResult = '';
   $scope.searchMovies = MovieService.factorySearchQuery;
-
-
+  $scope.movieResult = MovieService.movieResult;
+  // $scope.getResult = MovieService.getOMDB();
 
 }]);
 
 
+
 myApp.factory('MovieService', ['$http', function($http){
   var movie = movie;
-  var movieResult;
-  // var movieReturnObject;
+  var movieResult = [];
+  var favoriteMovies = [];
 
   var searchQuery = function(movieSearch){
     var movie = movieSearch;
@@ -31,8 +32,10 @@ myApp.factory('MovieService', ['$http', function($http){
     getOMDB(movie);
   };
 
+
   function getOMDB(movie){
     var OMDBmovie = movie;
+    console.log(OMDBmovie);
     $http.get('http://www.omdbapi.com/?t=' + OMDBmovie + '&y=&plot=short&r=json').then(function(response){
     var movieResultOMDB = {
         title: response.data.Title,
@@ -42,19 +45,36 @@ myApp.factory('MovieService', ['$http', function($http){
         plot: response.data.Plot,
         genre: response.data.Genre
       };
-      console.log("movieResult is: ", movieResultOMDB);
-      movieResult = movieResultOMDB;
+      // console.log("movieResult is: ", movieResultOMDB);
+      movieResult.push(movieResultOMDB);
       // movieReturnArray.push(movieResult);
       console.log(movieResult);
-
-
     });
   }
+  // console.log("Outside function: ", movieResult);
+
 
   return {
     movieResult: movieResult,
     factorySearchQuery: searchQuery,
-
+    // factoryGetOMDB: function(){
+    //   var OMDBmovie = movie;
+    //   console.log(OMDBmovie);
+    //   $http.get('http://www.omdbapi.com/?t=' + OMDBmovie + '&y=&plot=short&r=json').then(function(response){
+    //   var movieResultOMDB = {
+    //       title: response.data.Title,
+    //       runtime: response.data.Runtime,
+    //       year: response.data.Year,
+    //       director: response.data.Director,
+    //       plot: response.data.Plot,
+    //       genre: response.data.Genre
+    //     };
+    //     // console.log("movieResult is: ", movieResultOMDB);
+    //     movieResult = movieResultOMDB;
+    //     // movieReturnArray.push(movieResult);
+    //     console.log(movieResult);
+    //   });
+    // }
   };
 
 }]);
