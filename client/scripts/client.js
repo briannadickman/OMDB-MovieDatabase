f44a64cb8e13907ad30714d95d5b71e93780cff8
Bri Dickman <brianna.dickman@gmail.com>
2017-04-09 19:33:58 -0500

    search function complete, finalizing adding to DOM

--- client/scripts/client.js
+++ client/scripts/client.js
@@ -11,26 +11,50 @@ myApp.controller('SearchController', ['$scope', 'MovieService', function($scope,
 
   $scope.movieService = MovieService;
   $scope.movieSearch = '';
+  // $scope.movieResult = '';
+  $scope.searchMovies = MovieService.factorySearchQuery;
 
-  // var movie = $scope.movieSearch;
-  // $scope.getResult = MovieService.factoryGetResult();
 
-  $scope.getResult = function(){
-    var movie = $scope.movieSearch;
-    console.log(movie);
-    console.log('I was clicked!');
-
-  };
 
 }]);
 
 
 myApp.factory('MovieService', ['$http', function($http){
   var movie = movie;
+  var movieResult;
+  // var movieReturnObject;
+
+  var searchQuery = function(movieSearch){
+    var movie = movieSearch;
+    console.log(movie);
+    console.log('I was clicked!');
+    getOMDB(movie);
+  };
+
+  function getOMDB(movie){
+    var OMDBmovie = movie;
+    $http.get('http://www.omdbapi.com/?t=' + OMDBmovie + '&y=&plot=short&r=json').then(function(response){
+    var movieResultOMDB = {
+        title: response.data.Title,
+        runtime: response.data.Runtime,
+        year: response.data.Year,
+        director: response.data.Director,
+        plot: response.data.Plot,
+        genre: response.data.Genre
+      };
+      console.log("movieResult is: ", movieResultOMDB);
+      movieResult = movieResultOMDB;
+      // movieReturnArray.push(movieResult);
+      console.log(movieResult);
+
+
+    });
+  }
 
   return {
-    data: 'MovieService reached!',
-    movie: movie
+    movieResult: movieResult,
+    factorySearchQuery: searchQuery,
+
   };
 
 }]);
